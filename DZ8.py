94bb2c3e3e37dff5008fbbd293e965e47369e496
DmitroKO <99337185+DmitroKO@users.noreply.github.com>
2022-02-22 14:54:20 +0200

    Update DZ8.py

--- DZ8.py
+++ DZ8.py
@@ -1,32 +1,51 @@
 # пример 1
 lst = [0,1,2,3,4,5]
 print("Ваш список",lst)
-s = 0
+summa = 0
 for i in range(len(lst)):
     if i %2 ==0:
-        s = s + lst[i]*lst[-1]
-print(s)
+        summa = summa + lst[i]
+        number = lst[-1]
+        summa1 =summa * number
+print(summa1)
 print("END")
 print("-"* 30)
 
 # пример 2
 lst = [1,3,5]
 print("Ваш список",lst)
-s = 0
+summa = 0
 for i in range(len(lst)):
     if i %2 ==0:
-        s = s + lst[i]*lst[-1]
-print(s)
+        summa = summa + lst[i]
+        number = lst[-1]
+        summa1 = summa * number
+print(summa1)
 print("END")
 print("-"* 30)
 
-# пример 3
-lst = [0]
+# пример 4
+lst = [6]
 print("Ваш список",lst)
-s = 0
+summa = 0
 for i in range(len(lst)):
     if i %2 ==0:
-        s = s + lst[i]*lst[-1]
-print(s)
+        summa = summa + lst[i]
+        number = lst[-1]
+        summa1 = summa * number
+print(summa1)
+print("END")
+print("-"* 30)
+
+# пример 5
+lst = []
+print("Ваш список",lst)
+summa = 0
+for i in range(len(lst)):
+    if i %2 == 0:
+        summa = summa + lst[i]
+        number = lst[-1]
+        summa1 = summa * number
+    print(summa1)
 print("END")
 print("-"* 30)
